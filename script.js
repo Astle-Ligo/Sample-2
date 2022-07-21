60b94bfa2f57b32582253231f775ca5ff15f4295
Astle Ligo <astlelijo04@gmail.com>
2022-07-22 02:46:11 +0530

    blur ok

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var main = document.getElementById("main-content");
-var btn = document.getElementsByClassName("btn");
 var t = document.getElementById("top");
+var collapseWidthExample = document.getElementById("collapseWidthExample")
+var card = document.getElementById("card")
 function bg(){
-    if(btn.value=="NO"){
-        main.style.backgroundColor = "white"
+    if((collapseWidthExample.value=="NO")){
         main.style.opacity = "1"
-        btn.value="YES";
+        collapseWidthExample.value="YES";
     }
     else{
-        main.style.opacity = "0.3"
-        btn.value="NO"
+        main.style.opacity = "0.3";
+        collapseWidthExample.value="NO";
+        }
     }
-    
+window.addEventListener("scroll",scrly)
+function scrly(){
+    let offset = window.scrollY;
+    if(offset<=100)
+    t.style.display = "none";
+    else
+    t.style.display = "block"
 }
-
+ 
